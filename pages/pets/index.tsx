1c346dad88cfbdbff9bbefafd2a730a4c90ea5c3
Ben Wong <42486090+bwong365@users.noreply.github.com>
2022-10-23 03:27:53 -0600

    Update pet index page

--- pages/pets/index.tsx
+++ pages/pets/index.tsx
@@ -1,7 +1,8 @@
 import { ReactElement } from 'react';
-import { Box } from '@mantine/core';
+import { Box, Text, Title } from '@mantine/core';
 import { dehydrate } from '@tanstack/react-query';
 import { GetServerSideProps } from 'next';
+import Link from 'next/link';
 import { prefetchAllPets } from '@hooks/useFetchAllPets';
 import { PetLayout } from '@layouts/PetLayout';
 
@@ -16,7 +17,21 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
 };
 
 const Pets = () => {
-  return <Box>Welcome!</Box>;
+  return (
+    <Box>
+      <Title mb="xl" order={1}>
+        Welcome!
+      </Title>
+      <Text>Wet Pet helps you monitor the environment and relative comfort of your pets.</Text>
+      <Text mt="sm" size="sm">
+        *Powered by the
+        <Text span variant="link">
+          <Link href="https://openweathermap.org/api"> Open Weather Api </Link>
+        </Text>
+        for location validation and weather reporting
+      </Text>
+    </Box>
+  );
 };
 
 Pets.getLayout = (page: ReactElement) => {
